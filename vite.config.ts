042768c06e8bf01d723e6f35975d12f67a7e9f1c
Viacheslav Dmytriiev <v.dmytriiev@sdventures.com>
2026-02-09 16:04:53 +0100

    Added github workflow config to deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { name } from './package.json';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), tsconfigPaths()],
-});
+  base: command === 'build' ? `/${name}/` : '/',
+}));
